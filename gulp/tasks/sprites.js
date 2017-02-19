fe8684132855e02a93e32a10accb7059ff0af500
Erhun <Erhun@perronwei.nl>
2017-02-19 19:58:20 +0100

    Compledted header styling section.

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -4,11 +4,11 @@ rename = require('gulp-rename'),
 del = require('del');
 
 var config = {
-  mode:{
-    css:{
+  mode: {
+    css: {
       sprite: 'sprite.svg',
-      render:{
-        css:{
+      render: {
+        css: {
           template: './gulp/templates/sprite.css'
         }
       }
@@ -16,29 +16,29 @@ var config = {
   }
 }
 
-gulp.task('beginClean', function(){
+gulp.task('beginClean', function() {
   return del(['./app/temp/sprite', './app/assets/images/sprites']);
 });
 
-gulp.task('createSprite',['beginClean'], function () {
+gulp.task('createSprite', ['beginClean'], function() {
   return gulp.src('./app/assets/images/icons/**/*.svg')
     .pipe(svgSprite(config))
     .pipe(gulp.dest('./app/temp/sprite/'));
 });
 
-gulp.task('copySpriteGrapic', ['createSprite'], function(){
-  // Look for a solution for the css src folder to non css folder in dest.
-  return gulp.src('./app/temp/sprite/css/*.svg')
-  .pipe(gulp.dest('./app/assets/images/sprites'));
-})
+gulp.task('copySpriteGraphic', ['createSprite'], function() {
+  return gulp.src('./app/temp/sprite/css/**/*.svg')
+    .pipe(gulp.dest('./app/assets/images/sprites'));
+});
 
-gulp.task('coppySpriteCSS', ['createSprite'], function(){
+gulp.task('copySpriteCSS', ['createSprite'], function() {
   return gulp.src('./app/temp/sprite/css/*.css')
     .pipe(rename('_sprite.css'))
     .pipe(gulp.dest('./app/assets/styles/modules'));
 });
 
-gulp.task('endClean', ['copySpriteGrapic','coppySpriteCSS'], function(){
+gulp.task('endClean', ['copySpriteGraphic', 'copySpriteCSS'], function() {
   return del('./app/temp/sprite');
 });
-gulp.task('icons', ['beginClean','createSprite', 'copySpriteGrapic', 'coppySpriteCSS', 'endClean']);
+
+gulp.task('icons', ['beginClean', 'createSprite', 'copySpriteGraphic', 'copySpriteCSS', 'endClean']);
